771465316610ce223a93a484e6ae53fc20fa9f78
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-04-09 19:08:35 +0800

    refactored code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
-import Chat from './Chat'
-import Login from './Login'
-import Signup from './Signup'
-import './styles/App.css'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Chat from "./Chat";
+import Login from "./Login";
+import Signup from "./Signup";
+import "./styles/App.css";
 
 class App extends Component {
-	// Methods
-	// Render
-	render() {
-		return (
-			<BrowserRouter>
-				<Switch>
-					<Route path="/signup" component={Signup}></Route>
-					<Route path="/login" component={Login}></Route>
-					<Route
-						path="/"
-						render={props => {
-							//if gettting token from the local storage, render chat
-							return localStorage.getItem('token') ? (
-								<Chat {...props} />
-							) : (
-								//otherwise redirect to the login
-								<Redirect to="/login" />
-							)
-						}}
-					></Route>
-				</Switch>
-			</BrowserRouter>
-		)
-	}
+  // Methods
+  // Render
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signup" component={Signup}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route
+            path="/"
+            render={props => {
+              //if gettting token from the local storage, render chat
+              return localStorage.getItem("token") ? (
+                <Chat {...props} />
+              ) : (
+                //otherwise redirect to the login
+                <Redirect to="/login" />
+              );
+            }}
+          ></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
