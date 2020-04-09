f9bdbb55acec21b64deb5d138fb5f180bb2ce9d0
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-04-09 09:20:33 +0800

    corrections to the lifecycle

--- src/Messages.js
+++ src/Messages.js
@@ -34,6 +34,30 @@ class Messages extends Component {
       }
     );
   }
+
+  componentWillMount() {
+    this.setState(
+      {
+        selected: this.props.selected,
+        wallOpen: this.props.wallOpen
+      }, //async function, so do stuffs after this happened
+      () => {
+        let config = {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+        };
+        axios
+          .get(
+            `${process.env.REACT_APP_API}/messages?channel=${this.state.selected}`,
+            config
+          )
+          .then(res => {
+            res.data.reverse();
+            this.setState({ messages: res.data });
+          })
+          .catch(err => console.log(err));
+      }
+    );
+  }
   // Lifecycle
   componentWillReceiveProps(newProps) {
     //when the props inherited from Chat changed from [] to something
